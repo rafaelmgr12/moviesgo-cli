a84fd4cdc16bdfe6c2eb0789f5d55da16921bbe5
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-01-17 08:26:37 -0300

    [fix] properties in delete at

--- model/Movie.go
+++ model/Movie.go
@@ -4,8 +4,7 @@ import "gorm.io/gorm"
 
 type Movie struct {
 	gorm.Model
-	ID        string         `gorm:"primaryKey"`
-	Title     string         `json:"title" `
-	Genre     string         `json:"genre"`
-	DeletedAt gorm.DeletedAt `default:"null" json:"deleted_at"`
+	ID    string `gorm:"primaryKey"`
+	Title string `json:"title" `
+	Genre string `json:"genre"`
 }
