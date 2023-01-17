eab8c46b8e1eba437eaf1fe457c8214ee97ddb9e
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-01-17 20:57:33 -0300

    [feat] add commands

--- main.go
+++ main.go
@@ -1,57 +1,36 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"strings"
 
+	"github.com/rafaelmgr12/moviego-cli/controller"
+	"github.com/rafaelmgr12/moviego-cli/database"
+	"github.com/rafaelmgr12/moviego-cli/service"
 	"github.com/urfave/cli"
 )
 
 var app = cli.NewApp()
-var pizza = []string{"Enjoy your pizza with some delicious"}
 
 func info() {
-	app.Name = "Simple pizza CLI"
-	app.Usage = "An example CLI for ordering pizza's"
-	app.Author = "Jeroenouw"
+	app.Name = "Moviego CLI"
+	app.Usage = "A CLI for inserting data into a database using a CSV file"
+	app.Author = "rafaelmgr12"
 	app.Version = "1.0.0"
 }
-
 func commands() {
 	app.Commands = []cli.Command{
 		{
-			Name:    "peppers",
-			Aliases: []string{"p"},
-			Usage:   "Add peppers to your pizza",
+			Name:    "insert",
+			Aliases: []string{"i"},
+			Usage:   "Add a movie to the database",
 			Action: func(c *cli.Context) {
-				pe := "peppers"
-				peppers := append(pizza, pe)
-				m := strings.Join(peppers, " ")
-				fmt.Println(m)
-			},
-		},
-		{
-			Name:    "pineapple",
-			Aliases: []string{"pa"},
-			Usage:   "Add pineapple to your pizza",
-			Action: func(c *cli.Context) {
-				pa := "pineapple"
-				pineapple := append(pizza, pa)
-				m := strings.Join(pineapple, " ")
-				fmt.Println(m)
-			},
-		},
-		{
-			Name:    "cheese",
-			Aliases: []string{"c"},
-			Usage:   "Add cheese to your pizza",
-			Action: func(c *cli.Context) {
-				ch := "cheese"
-				cheese := append(pizza, ch)
-				m := strings.Join(cheese, " ")
-				fmt.Println(m)
+				database.Connect()
+				movies := service.GetAllMoviesFromCSV("./input/movies.csv")
+				for _, movie := range movies {
+					controller.SaveMovieInDB(movie)
+				}
+
 			},
 		},
 	}
