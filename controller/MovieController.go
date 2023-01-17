d9d0661af267ede0cb67543421e170307218404c
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-01-17 08:52:50 -0300

    [feat] save into the database

--- controller/MovieController.go
+++ controller/MovieController.go
@@ -1,5 +1,12 @@
 package controller
 
-func SaveMovieInDB() {
+import (
+	"github.com/rafaelmgr12/moviego-cli/database"
+	"github.com/rafaelmgr12/moviego-cli/model"
+)
+
+func SaveMovieInDB(movie model.Movie) {
+	database.Connect()
+	database.DB.Create(&movie)
 
 }
