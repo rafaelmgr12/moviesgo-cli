568f698e9709664b6b943c35d31f27e9ec7a8495
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-01-17 08:10:27 -0300

    [fix] error in the delete_at collumn

--- model/Movie.go
+++ model/Movie.go
@@ -4,7 +4,8 @@ import "gorm.io/gorm"
 
 type Movie struct {
 	gorm.Model
-	ID    string `gorm:"primaryKey"`
-	Title string
-	Genre string
+	ID        string         `gorm:"primaryKey"`
+	Title     string         `json:"title" `
+	Genre     string         `json:"genre"`
+	DeletedAt gorm.DeletedAt `default:"null" json:"deleted_at"`
 }
