1f7eb5c064baac769ddb29b4a8464da34e93e8fe
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-01-17 21:12:59 -0300

    [fix] ignoring the colluns and transformi id into int type

--- service/MovieService.go
+++ service/MovieService.go
@@ -1,20 +1,32 @@
 package service
 
 import (
+	"strconv"
+
 	"github.com/rafaelmgr12/moviego-cli/model"
 	"github.com/rafaelmgr12/moviego-cli/utils"
 )
 
 func GetAllMoviesFromCSV(filePath string) []model.Movie {
 	records := utils.ReadCSVFile(filePath)
+	nameExistMap := make(map[string]bool)
+
 	var movies []model.Movie
-	for _, record := range records {
+	for _, record := range records[1:] {
+		id, err := strconv.Atoi(record[0])
+		if err != nil {
+			panic(err)
+		}
 		movie := model.Movie{
-			ID:    record[0],
+			ID:    id,
 			Title: record[1],
 			Genre: record[2],
 		}
-		movies = append(movies, movie)
+		if _, ok := nameExistMap[movie.Title]; ok {
+			continue
+		} else {
+			movies = append(movies, movie)
+		}
 	}
 	return movies
 }
